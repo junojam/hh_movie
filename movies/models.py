d03b95abdd073e5a95bf2091c162cb7f63447d31
이준호 <nak3652@naver.com>
2022-05-20 13:18:39 +0900

    final-pjt-day1-01

---
+++ movies/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Movie(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
---
+++ movies/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Movie, Comment
+from .forms import MovieForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST, require_GET
+
+# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    context={
+        'movies':movies
+    }
+
+    return render(request, 'movies/index.html',context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            movie = form.save(commit=False)
+            movie.user = request.user
+            movie.save()
+            return redirect('movies:detail', movie.pk)
+    
+    else:
+        form=MovieForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'movies/create.html', context)
+
+
+def detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    comment_form=CommentForm()
+    comments=movie.comment_set.all()
+    context = {
+        'movie':movie,
+        'comment_form':comment_form,
+        'comments':comments,
+    }
+    return render(request, 'movies/detail.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method=='POST':
+        form = MovieForm(request.POST, instance=movie)
+        if form.is_valid():
+            movie = form.save()
+            return redirect('movies:detail', movie.pk)
+    else:
+        form = MovieForm(instance=movie)
+    context={
+        'movie':movie,
+        'form':form,
+    }
+
+    return render(request, 'movies/update.html', context)
+
+@require_POST
+def delete(request, pk):
+    if request.user.is_authenticated:
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.delete()
+    return redirect('movies:index')
+
+@require_POST
+def comments_create(request, pk):
+    if request.user.is_authenticated:
+        movie = get_object_or_404(Movie, pk=pk)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment=comment_form.save(commit=False)
+            comment.movie=movie
+            comment.user = request.user
+            comment.save()
+        return redirect('movies:detail', movie.pk)
+    return redirect('accounts:login')
+
+@require_POST
+def comments_delete(request, movie_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment=get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.user:
+            comment.delete()
+    return redirect('movies:detail', movie_pk)
